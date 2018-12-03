b5044037498cf2ed65553bf49e53ef3edddbf6d8
AppTemon <AppTemon@Gmail.com>
2018-12-03 17:51:35 +0300

    GetComponent optimisation

--- Assets/PoolsManager/Scripts/Pool.cs
+++ Assets/PoolsManager/Scripts/Pool.cs
@@ -2,16 +2,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace PoolsManagement {
-    public class Pool {
+    public class Pool{
         public MegaPool megaPool;
         public Transform holder;
         public PoolSettingsSO poolSet;
         public Queue<IPoolable> poolables = new Queue<IPoolable>();
 
+        private readonly bool prefabHasIPoolable;
+
         public Pool(MegaPool _megaPool, PoolSettingsSO _poolSet, Transform _holder) {
             megaPool = _megaPool;
             holder = _holder;
             poolSet = _poolSet;
+            prefabHasIPoolable = (poolSet.prefab.GetComponent<IPoolable>() != null);
             for (int i = 0; i < poolSet.initialSize; i++) {
                 poolables.Enqueue(CreateNewPoolable());
             }
@@ -34,14 +37,15 @@ namespace PoolsManagement {
 
         IPoolable CreateNewPoolable() {
             GameObject newObj = GameObject.Instantiate(poolSet.prefab, holder);
-            IPoolable poolable = newObj.GetComponent<PoolableObject>();
-            if (poolable == null) {
+            IPoolable poolable = null;
+            if (prefabHasIPoolable) {
                 poolable = newObj.GetComponent<IPoolable>();
                 if (poolable == null) {
                     poolable = newObj.AddComponent<PoolableObject>();
                 }
+            } else {
+                poolable = newObj.AddComponent<PoolableObject>();
             }
-
             poolable.InitializePoolable(poolSet);
             return poolable;
         }
